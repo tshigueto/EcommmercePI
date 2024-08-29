e6b5262463ebdeac978a5fcd081ff8e07510d64e
Thiago Shigueto Hossaka <126188799+tshigueto@users.noreply.github.com>
2024-08-29 10:28:39 -0300

    Commit new file - strategy

---
+++ src/main/java/br/com/tads/ecommerce/strategy/MediumPriceShippingStrategy.java
@@ -0,0 +1,29 @@
+/*
+    Nome do Projeto: E-commerce
+    Data de Criação: 15/02/2024
+    Versão: 5
+    Data da Última Modificação: 22/05/2024
+    Versão do Java: 17
+    Versão do Spring Boot: 3.2.4
+    Banco de dados: H2
+    Equipe de Desenvolvimento:
+        - Juan Souza Santos
+        - Maria Helena dos Santos
+        - Pedro Ferreira Lima
+        - Thiago Shigueto Hossaka
+
+    Descrição: Este programa é uma plataforma de e-commerce destinada à venda de produtos variados.
+        A aplicação permite aos usuários navegar por uma lista de produtos, adicionar itens ao carrinho
+        de compras, calcular o custo total incluindo frete e finalizar a compra.
+
+    Funcionalidades incluem cadastro e login de usuários, exibição de produtos, cálculo de preços,
+        gerenciamento do carrinho de compras, checkout e armazenamento dos resultados em um banco de dados.
+*/
+
+package br.com.tads.ecommerce.strategy;
+public class MediumPriceShippingStrategy implements ShippingStrategy {
+    @Override
+    public double calculateShipping(double totalPrice) {
+        return 10.0;
+    }
+}
