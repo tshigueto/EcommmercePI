901ed9392c37d1d51bce756edfa8d77e76d26a2c
Thiago Shigueto Hossaka <126188799+tshigueto@users.noreply.github.com>
2024-08-29 10:22:07 -0300

    Commit new files - Repository

---
+++ src/main/java/br/com/tads/ecommerce/repository/ProductImageRepository.java
@@ -0,0 +1,34 @@
+/*
+    Nome do Projeto: E-commerce
+    Data de Criação: 15/02/2024
+    Versão: 5
+    Data da Última Modificação: 22/05/2024
+    Versão do Java: 17
+    Versão do Spring Boot: 3.2.4
+    Banco de dados: H2
+    Equipe de Desenvolvimento:
+        - Juan Souza Santos
+        - Maria Helena dos Santos
+        - Pedro Ferreira Lima
+        - Thiago Shigueto Hossaka
+
+    Descrição: Este programa é uma plataforma de e-commerce destinada à venda de produtos variados.
+        A aplicação permite aos usuários navegar por uma lista de produtos, adicionar itens ao carrinho
+        de compras, calcular o custo total incluindo frete e finalizar a compra.
+
+    Funcionalidades incluem cadastro e login de usuários, exibição de produtos, cálculo de preços,
+        gerenciamento do carrinho de compras, checkout e armazenamento dos resultados em um banco de dados.
+*/
+
+package br.com.tads.ecommerce.repository;
+
+import br.com.tads.ecommerce.model.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+    List<ProductImage> findByProductId(Long productId);
+}
